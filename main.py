5d7a738bc2753aba45843e8a12d43b0377ef7312
JIVESH HAMAL <bravojivesh@gmail.com>
2024-03-18 00:39:35 +0530

    Kanye quotes working. Others WIP

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import requests
 
 api=requests.get(url="http://api.open-notify.org/iss-now.json")
-#this only stores the response such as 200.Returns the object.
+#this only stores the response object such as 200.Returns the object.
 
-api.raise_for_status() #method to directly show us HTTP error. Without this we get JSON decoder error
-#etc.
+api.raise_for_status()
+#If the status code of the HTTP response is in the range 400-599 (client error or server error),
+# raise_for_status() will raise an instance of requests.HTTPError with an appropriate message.
+# This allows you to handle error cases in a clean and explicit way.
 
-print (api)
+print (api) #this PRINTS the response object.
 print (api.status_code) #returns only the response code
 print(api.json()) #returns the json data of the api
 
